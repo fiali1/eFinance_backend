6fe61f06712d96c5c204e6f9f114787601befe7c
Gabriel Fiali <fiali1@hotmail.com>
2022-09-06 12:16:50 -0300

    heroku deployent#6

--- src/index.ts
+++ src/index.ts
@@ -3,4 +3,6 @@ import { app } from './server';
 
 config();
 
-app.listen(3333, () => console.log('Server running on port 3333...'));
+app.listen(process.env.PORT, () =>
+  console.log(`Server running on port ${process.env.PORT}...`)
+);
